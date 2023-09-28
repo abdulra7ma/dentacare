b136291c61e41e659a961c2831f673dab01abfca
abdulrahmandawoud <dawoud.abdulrahman.04@gmail.com>
2023-09-29 04:45:43 +0600

    added models and formatted templates

--- health/models.py
+++ health/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        """
+        Returns a string representation of the object.
+        """
+        return self.name
+
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    department = models.CharField(
+        max_length=100,
+        choices=[
+            ("Teeth Whiteing", "teeth_whiteing"),
+            ("Teeth Cleaning", "teeth_cleaning"),
+            ("Quality Brackets", "quality_brackets"),
+            ("Modern Anesthetic", "modern_anesthetic"),
+        ],
+    )
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    def __str__(self):
+        """
+        Returns a string representation of the object's name.
+
+        :return: The name of the object as a string.
+        """
+        return self.name
--- health/views.py
+++ health/views.py
@@ -1,6 +1,16 @@
+from django import views
 from django.shortcuts import render
+from django.views import generic
+
 
 def home(request):
-    return render(request,'home.html',{})
+    return render(request, "home.html", {})
+
+
+class HomeView(generic.TemplateView):
+    template_name = "home.html"
+    
+
+
 def contact(request):
-    return render(request,'contact.html',{})
+    return render(request, "contact.html", {})
